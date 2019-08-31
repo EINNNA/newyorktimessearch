d4d365607524b805914b10632f6bbdc63df8e232
Kelliott <52241744+kelliott14@users.noreply.github.com>
2019-08-31 14:05:41 +1000

    working better

--- java.js
+++ java.js
@@ -4,18 +4,11 @@ $(document).ready(function() {
         e.preventDefault();
 
 var searchWord = $("#search").val();
-var recordNumber = $("#exampleFormControlSelect1").val();
+var recordNumber = parseInt($("#exampleFormControlSelect1").val());
 var startYear = $("#startYear").val();
 var endYear = $("#endYear").val();
 
-
-
 var queryURL;
-
-"https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" 
-+ searchWord + "&api-key=oazeg0GfU32cSpc3tG0bBAugQH9u3dy9&limit=" 
-+ recordNumber + "begin_date=" + startYear + "0101&end_date=" + endYear + "1231";
-
 
 function buildURL(){
     queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" 
@@ -41,19 +34,26 @@ $.ajax({
 	url: queryURL,
 	method: "GET"
 }).then(function(response){
-    
+    $("#articleResults").empty();
     var result = response.response.docs;
-    console.log(response);
-    var title = result[0].headline.main;
-    var source = result[0].source;
-    var url = result[0].web_url;
+    
+    for (var i = 0; i < recordNumber; i++){
+        
 
-    var articleDiv = $("<div>");
+        var title = result[i].headline.main;
+        var source = result[i].source;
+        var url = result[i].web_url;
 
-    $(articleDiv).append("<a href='" + url + "' target='_blank'>" + title + "</a>");
-    $(articleDiv).append("<p>" + source + "</p>");
+        var articleDiv = $("<div>");
 
-    $("#articleResults").append(articleDiv);
+
+        $(articleDiv).append("<a href='" + url + "' target='_blank'>" + title + "</a>");
+        $(articleDiv).append("<p>" + source + "</p>");
+
+        $("#articleResults").append(articleDiv);
+
+    }
+    
 
 
 
